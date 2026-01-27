9c1fa6f907adbebc2fb175ef652cfa8785342c42
Sri Surya Tarun Narala <surya@Sris-MacBook-Pro.local>
2026-01-27 14:17:08 -0500

    refactor: upgrade rate limiter from fixed window to sliding window algorithm using Redis sorted sets

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,23 +1,44 @@
-# Fixed Window
-import redis 
+# # Fixed Window
+# import redis 
+# from config import REDIS_HOST, REDIS_PORT, RATE_LIMIT, TIME_WINDOW
+
+# r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
+# def is_allowed(client_id: str):
+#     key = f"fw:{client_id}"
+
+#     current = r.get(key)
+
+#     if current is None:
+#         # first request in window
+#         r.set(key, 1, ex=TIME_WINDOW)
+#         return True
+
+#     if int(current) < RATE_LIMIT:
+#         r.incr(key)
+#         return True
+
+#     return False
+
+
+# sliding_window.py
+import time
+import redis
 from config import REDIS_HOST, REDIS_PORT, RATE_LIMIT, TIME_WINDOW
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 def is_allowed(client_id: str):
-    key = f"fw:{client_id}"
+    key = f"rate_limit:{client_id}"
+    now = time.time()
 
-    current = r.get(key)
+    pipe = r.pipeline()
 
-    if current is None:
-        # first request in window
-        r.set(key, 1, ex=TIME_WINDOW)
-        return True
+    pipe.zremrangebyscore(key, 0, now - TIME_WINDOW)
+    pipe.zcard(key)
+    pipe.zadd(key, {str(now): now})
+    pipe.expire(key, TIME_WINDOW)
 
-    if int(current) < RATE_LIMIT:
-        r.incr(key)
-        return True
+    _, request_count, _, _ = pipe.execute()
 
-    return False
-
-
+    return request_count < RATE_LIMIT
